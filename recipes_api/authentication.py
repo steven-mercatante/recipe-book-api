be062ad41299db7ee0fbb00c262f1af2263ae806
Steven Mercatante <steven.mercatante@gmail.com>
2022-01-11 21:45:10 -0500

    Update authentication.py

--- recipes_api/authentication.py
+++ recipes_api/authentication.py
@@ -1,25 +1,46 @@
 # See: https://auth0.com/docs/quickstart/backend/python/01-authorization#validate-access-tokens
 import os
+import logging
 
 import requests
 from jose import jwt
-from rest_framework.authentication import BaseAuthentication
+from rest_framework.authentication import BaseAuthentication, TokenAuthentication
+from django.contrib.auth.models import AnonymousUser
+from django.urls import resolve
 
 from users.models import User
 
-"""
-TODO: make sure the API returns whatever status code is set in the 
-exception. The below code passes in 401, but Postman is getting 500
-"""
 
 
 class AuthError(Exception):
     pass
 
 
+class RecipeDetailsAuthentication(object):
+    """
+    This authentication class returns an AnonymousUser when
+    GETting a single Recipe to view, since viewing single Recipes
+    should be publically available.
+    """
+    def authenticate(self, request):
+        url_name = resolve(request.path).url_name
+        # Note: 'recipes-detail' is created by DRF's SimpleRouter.
+        # This must be updated if the "recipes" basename changes.
+        if url_name == 'recipes-detail' and request.method == 'GET':
+            return AnonymousUser, None
+        return None
+
+
 # TODO: test
 class DRFAuth0Authentication(BaseAuthentication):
     def authenticate(self, request):
+        try:
+            self._authenticate(request)
+        except AuthError as e:
+            logging.error(e)
+            return None
+
+    def _authenticate(self, request):
         auth0_domain = os.getenv('DJANGO_AUTH0_DOMAIN')
         api_audience = os.getenv('DJANGO_AUTH0_AUDIENCE')
         algorithms = ["RS256"]
@@ -45,55 +66,41 @@ class DRFAuth0Authentication(BaseAuthentication):
                     rsa_key,
                     algorithms=algorithms,
                     audience=api_audience,
-                    issuer="https://" + auth0_domain + "/"
+                    issuer=f"https://{auth0_domain}/"
                 )
             except jwt.ExpiredSignatureError:
-                raise AuthError({"code": "token_expired",
-                                 "description": "token is expired"}, 401)
+                raise AuthError("token is expired")
             except jwt.JWTClaimsError:
-                raise AuthError({"code": "invalid_claims",
-                                 "description":
-                                     "incorrect claims,"
-                                     "please check the audience and issuer"}, 401)
+                raise AuthError("incorrect claims, please check the audience and issuer")
             except Exception:
-                raise AuthError({"code": "invalid_header",
-                                 "description":
-                                     "Unable to parse authentication"
-                                     " token."}, 401)
+                raise AuthError("Unable to parse authentication token.")
 
             user_email = payload.get('https://recipes/email')
             (user, _) = User.objects.get_or_create(email=user_email, defaults={
                 'username': user_email,
             })
             return user, True
-        raise AuthError({"code": "invalid_header",
-                         "description": "Unable to find appropriate key"}, 401)
+        raise AuthError("Unable to find appropriate key")
 
 
 # Format error response and append status code
 def get_token_auth_header(request):
-    """Obtains the Access Token from the Authorization Header
+    """
+    Obtains the Access Token from the Authorization Header
     """
     auth = request.headers.get("Authorization", None)
 
     if not auth:
-        raise AuthError({"code": "authorization_header_missing",
-                         "description":
-                             "Authorization header is expected"}, 401)
+        raise AuthError("Authorization header is expected")
 
     parts = auth.split()
 
     if parts[0].lower() != "bearer":
-        raise AuthError({"code": "invalid_header",
-                         "description":
-                             "Authorization header must start with Bearer"}, 401)
+        raise AuthError("Authorization header must start with Bearer")
     elif len(parts) == 1:
-        raise AuthError({"code": "invalid_header",
-                         "description": "Token not found"}, 401)
+        raise AuthError("Token not found")
     elif len(parts) > 2:
-        raise AuthError({"code": "invalid_header",
-                         "description":
-                             "Authorization header must be Bearer token"}, 401)
+        raise AuthError("Authorization header must be Bearer token")
 
     token = parts[1]
     return token
